943cceaae5373337dad475f7002f79a9efb99a3f
Tishtanya Shaw <Tishtanyaa@icloud.com>
2023-11-20 13:25:24 -0500

    Update HomeScreen, added components for HomeScreen, added logo and icon to assets, and added json data

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -7,28 +7,40 @@ const Layout = () => {
 		<Tabs
 			screenOptions={{
 				headerShown: false,
-				height: 70,
+				tabBarStyle: { height: 80 },
 			}}
 		>
 			<Tabs.Screen
 				name='one'
 				options={{
 					tabBarLabel: 'Home',
-					tabBarIcon: () => <Ionicons name='home' size={24} />,
+					tabBarIcon: () => <Ionicons name='home' size={30} />,
+					tabBarLabelStyle: {
+						marginBottom: 20,
+						fontSize: 14,
+					},
 				}}
 			/>
 			<Tabs.Screen
 				name='two'
 				options={{
 					tabBarLabel: 'Search',
-					tabBarIcon: () => <Ionicons name='search' size={24} />,
+					tabBarIcon: () => <Ionicons name='search' size={30} />,
+					tabBarLabelStyle: {
+						marginBottom: 20,
+						fontSize: 14,
+					},
 				}}
 			/>
 			<Tabs.Screen
 				name='three'
 				options={{
 					tabBarLabel: 'Locate',
-					tabBarIcon: () => <Ionicons name='locate' size={24} />,
+					tabBarIcon: () => <Ionicons name='locate' size={30} />,
+					tabBarLabelStyle: {
+						marginBottom: 20,
+						fontSize: 14,
+					},
 				}}
 			/>
 			<Tabs.Screen
@@ -36,8 +48,12 @@ const Layout = () => {
 				options={{
 					tabBarLabel: 'Faves',
 					tabBarIcon: () => (
-						<FontAwesome5 name='heart' size={24} color='black' />
+						<FontAwesome5 name='heart' size={30} color='black' />
 					),
+					tabBarLabelStyle: {
+						marginBottom: 20,
+						fontSize: 14,
+					},
 				}}
 			/>
 			<Tabs.Screen
@@ -45,8 +61,12 @@ const Layout = () => {
 				options={{
 					tabBarLabel: 'Me',
 					tabBarIcon: () => (
-						<FontAwesome name='user-circle-o' size={24} color='black' />
+						<FontAwesome name='user-circle-o' size={30} color='black' />
 					),
+					tabBarLabelStyle: {
+						marginBottom: 20,
+						fontSize: 14,
+					},
 				}}
 			/>
 		</Tabs>
